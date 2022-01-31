4d1ef0c5b85ef06f9f57a60439bb9ee165662253
SmithaVerity <smitha@theveritycorp.com>
2022-02-01 01:44:41 +0530

    Update acr_create.sh

--- acr_create.sh
+++ acr_create.sh
@@ -1,9 +1,10 @@
-az acr create -g k8demo --sku Standard --name k8demoCR010222
+ACR_NAME = k8demoCR010222
+az acr create -g k8demo --sku Standard --name $ACR_NAME
 
 cd ratings-api
-az acr build  -g k8demo --registry k8demoCR010222  --image ratings-api:v1 .
+az acr build  -g k8demo --registry $ACR_NAME  --image ratings-api:v1 .
 
 cd ~
 cd ratings-web
-az acr build -g k8demo --registry k8demoCR010222  --image ratings-web:v1 .
-az acr repository list  --name k8demoCR010222   --output table
+az acr build -g k8demo --registry $ACR_NAME  --image ratings-web:v1 .
+az acr repository list  --name $ACR_NAME   --output table
