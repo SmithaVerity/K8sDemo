6ffe81563d0842f4763ad47dca3e6a08b421c68b
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-03 01:39:00 +0530

    Delete acr_create.sh

--- acr_create.sh
+++
@@ -1,9 +0,0 @@
-
-ACR_NAME=acrVerityk8sdemo
-
-cd ratings-api
-az acr build  -g k8demo --registry $ACR_NAME  --image ratings-api:v1 .
-cd ~
-cd ratings-web
-az acr build -g k8demo --registry $ACR_NAME  --image ratings-web:v1 .
-az acr repository list  --name $ACR_NAME   --output table
