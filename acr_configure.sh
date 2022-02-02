e8649a4caef3dabc888d602e41459a54da21a06d
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-03 02:08:00 +0530

    Update acr_configure.sh

--- acr_configure.sh
+++ acr_configure.sh
@@ -1,2 +1,10 @@
+AKS_CLUSTER_NAME=k8demoCluster
+ACR_NAME=acrVerityk8sdemo
+az acr create  -g k8demo  -n $ACR_NAME  --sku Standard
 cd ratings-api
 az acr build  -g k8demo --registry acrVerityk8sdemo  --image ratings-api:v1 .
+cd ~
+cd ratings-web
+az acr build -g k8demo --registry acrVerityk8sdemo  --image ratings-web:v1 .
+az acr repository list  --name acrVerityk8sdemo   --output table
+az aks update --name $AKS_CLUSTER_NAME  -g k8demo --attach-acr $ACR_NAME
