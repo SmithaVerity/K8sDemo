9e08986f3bbb9680f7857e74e03ee274256e1da5
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-04 12:36:23 +0530

    Update ratings_api.sh

--- ratings_api.sh
+++ ratings_api.sh
@@ -1,1 +1,3 @@
 kubectl apply --namespace ratingsapp -f ratings-api-deployment.yaml
+kubectl get pods --namespace ratingsapp -l app=ratings-api -w
+kubectl get deployment ratings-api --namespace ratingsapp
