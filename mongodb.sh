63b545cedb9ece026def417a1fc2bcb6f6f52cf1
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-04 21:35:31 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -2,6 +2,3 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm search repo bitnami
 helm install ratings bitnami/mongodb  --namespace ratingsapp \
     --set auth.username=cldtraining,auth.password=cldtraining,auth.database=ratingsdb
-kubectl create secret generic mongosecret --namespace ratingsapp \
-    --from-literal=MONGOCONNECTION="mongodb://cldtraining:cldtraining@ratings-mongodb.ratingsapp:27017/ratingsdb"
-kubectl describe secret mongosecret --namespace ratingsapp
