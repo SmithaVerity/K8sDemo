ac4b4624a3666e5288ac52e5d0f9d3376e1bb06b
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-04 21:36:34 +0530

    Create secret_mongo.sh

---
+++ secret_mongo.sh
@@ -0,0 +1,3 @@
+kubectl create secret generic mongosecret --namespace ratingsapp \
+    --from-literal=MONGOCONNECTION="mongodb://cldtraining:cldtraining@ratings-mongodb.ratingsapp:27017/ratingsdb"
+kubectl describe secret mongosecret --namespace ratingsapp
