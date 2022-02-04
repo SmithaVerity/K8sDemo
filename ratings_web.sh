4f4ce1fa4dbf5925a920ff390b587a0a37b3be86
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-04 14:55:21 +0530

    Create ratings_web.sh

---
+++ ratings_web.sh
@@ -0,0 +1,6 @@
+kubectl apply --namespace ratingsapp -f ratings-web-deployment.yaml
+kubectl get pods --namespace ratingsapp -l app=ratings-web -w
+kubectl get deployment ratings-web --namespace ratingsapp
+kubectl apply --namespace ratingsapp -f ratings-web-service.yaml
+kubectl get service ratings-web --namespace ratingsapp
+kubectl get endpoints ratings-web --namespace ratingsapp
