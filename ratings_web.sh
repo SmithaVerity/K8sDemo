33cfc321c1eee80855de2703ff96609eeed10e9c
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-04 14:57:22 +0530

    Update ratings_web.sh

--- ratings_web.sh
+++ ratings_web.sh
@@ -2,5 +2,5 @@ kubectl apply --namespace ratingsapp -f ratings-web-deployment.yaml
 kubectl get pods --namespace ratingsapp -l app=ratings-web -w
 kubectl get deployment ratings-web --namespace ratingsapp
 kubectl apply --namespace ratingsapp -f ratings-web-service.yaml
-kubectl get service ratings-web --namespace ratingsapp
+kubectl get service ratings-web --namespace ratingsapp -w
 kubectl get endpoints ratings-web --namespace ratingsapp
