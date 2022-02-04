1affa901479a9a0810439f1284a0caab0a175e30
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-04 16:36:39 +0530

    Update and rename secret_mongo.sh to ingress.sh

---
+++ ingress.sh
@@ -0,0 +1,8 @@
+kubectl create namespace ingress
+helm repo add ingress-nginx ingress-nginx
+helm repo update
+helm install nginx-ingress ingress-nginx/ingress-nginx  --namespace ingress \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
+kubectl get services --namespace ingress -w
