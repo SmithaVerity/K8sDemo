4aa46e1e3989b2d0d5c18ac73d8f879b375c5345
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-05 12:05:38 +0530

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,9 +1,9 @@
 az resource create --resource-type Microsoft.OperationalInsights/workspaces \
-        --name k8demoworkspace  --resource-group $RESOURCE_GROUP  --location eastus \
+        --name k8demoworkspace  -g k8demo  --location eastus \
         --properties '{}' -o table
 
 WORKSPACE_ID=$(az resource show --resource-type Microsoft.OperationalInsights/workspaces \
-    --resource-group $RESOURCE_GROUP \
+    -g k8demo \
     --name k8demoworkspace \
     --query "id" -o tsv)
 
