6de1186acdf43b5840e96c57cad0248277f9e971
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-02-05 20:12:24 +0530

    Create load_apply.sh

---
+++ load_apply.sh
@@ -0,0 +1,2 @@
+LOADTEST_API_ENDPOINT=https://frontend.20.62.212.11.nip.io/api/loadtest		
+az container create -g k8demo -n loadtest --cpu 4 --memory 1 --image azch/artillery --restart-policy Never --command-line "artillery quick -r 500 -d 120 $LOADTEST_API_ENDPOINT"
