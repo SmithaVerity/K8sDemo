095cbc4db317a8356c1bb54394fff64efdd6f74a
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-11 21:31:38 +0530

    Update 6_ratings_api.sh

--- 6_ratings_api.sh
+++ 6_ratings_api.sh
@@ -1,3 +1,5 @@
+#Check ratings-api-deployment.yaml file before excecuting this code
+#IMPORTANT: update the Azure Container registry path with your own repository
 kubectl apply --namespace ratingsapp -f ratings-api-deployment.yaml
 kubectl get pods --namespace ratingsapp -l app=ratings-api -w
 kubectl get deployment ratings-api --namespace ratingsapp
