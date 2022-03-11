9e7f4320f8c7bd3acc2e5ee8e332d686d972b6fa
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-11 21:47:07 +0530

    Update 7_ratings_web.sh

--- 7_ratings_web.sh
+++ 7_ratings_web.sh
@@ -1,3 +1,5 @@
+#Check ratings-web-deployment.yaml file before excecuting this code
+#IMPORTANT: update the Azure Container registry path with your own repository
 kubectl apply --namespace ratingsapp -f ratings-web-deployment.yaml
 kubectl get pods --namespace ratingsapp -l app=ratings-web -w
 kubectl get deployment ratings-web --namespace ratingsapp
