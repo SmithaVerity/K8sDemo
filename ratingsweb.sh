1baf872ccc96033e756d70a6db3aac34a15eb599
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-11 13:38:53 +0530

    Delete ratingsweb.sh

--- ratingsweb.sh
+++
@@ -1,6 +0,0 @@
-kubectl apply --namespace ratingsapp -f ratings-web-deployment.yaml
-kubectl get pods --namespace ratingsapp -l app=ratings-web -w
-kubectl get deployment ratings-web --namespace ratingsapp
-
-kubectl apply  --namespace ratingsapp   -f ratings-web-service.yaml
-kubectl get service ratings-web --namespace ratingsapp -w
