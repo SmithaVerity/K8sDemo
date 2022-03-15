96c7108a4fb08b8ee453a94f1387379e06ca57c2
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-15 16:37:22 +0530

    Update 10_apply_ingress.sh

--- 10_apply_ingress.sh
+++ 10_apply_ingress.sh
@@ -1,4 +1,5 @@
 # ratings-web-service.yaml Change the last line service type to ClusterIP.
+# ratings-web-ingress.yaml Change IP address
 kubectl delete service  --namespace ratingsapp  ratings-web
 kubectl apply  --namespace ratingsapp  -f ratings-web-service.yaml
 kubectl apply  --namespace ratingsapp  -f ratings-web-ingress.yaml
