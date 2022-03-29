476b6efd1c6f6b1f950aae8962d0c257a5659624
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-29 15:57:46 +0530

    Update 1_aks_create.sh

--- 1_aks_create.sh
+++ 1_aks_create.sh
@@ -8,7 +8,8 @@ WORKSPACE=k8demoworkspace
 
 
 az group create   --name $RESOURCE_GROUP   --location $REGION_NAME
-az network vnet create  --resource-group $RESOURCE_GROUP   --location $REGION_NAME   --name $VNET_NAME  --address-prefixes 10.0.0.0/8  --subnet-name $SUBNET_NAME   --subnet-prefixes 10.240.0.0/16
+az network vnet create  --resource-group $RESOURCE_GROUP   --location $REGION_NAME   --name $VNET_NAME  --address-prefixes 10.0.0.0/24 \ 
+--subnet-name $SUBNET_NAME   --subnet-prefixes 10.0.0.0/29
 SUBNET_ID=$(az network vnet subnet show   --resource-group $RESOURCE_GROUP  --vnet-name $VNET_NAME --name $SUBNET_NAME  --query id -o tsv)	
 VERSION=$(az aks get-versions  --location $REGION_NAME  --query 'orchestrators[?!isPreview] | [-1].orchestratorVersion'   --output tsv)
 
@@ -22,8 +23,8 @@ az aks create \
 --kubernetes-version $VERSION \
 --network-plugin azure \
 --vnet-subnet-id $SUBNET_ID \
---service-cidr 10.2.0.0/24 \
---dns-service-ip 10.2.0.10 \
+--service-cidr 10.0.0.0/24 \
+--dns-service-ip 10.0.0.10 \
 --docker-bridge-address 172.17.0.1/16 \
 --generate-ssh-keys --y
 
