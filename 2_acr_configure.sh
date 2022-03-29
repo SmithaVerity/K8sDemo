ffcc69d67f2729d452c73b2168ba0945949abc5b
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-29 19:05:02 +0530

    Update 2_acr_configure.sh

--- 2_acr_configure.sh
+++ 2_acr_configure.sh
@@ -1,12 +1,12 @@
-AKS_CLUSTER_NAME=k8demoCluster
-ACR_NAME=acrVerityk8sdemo
-az acr create  -g k8demo  -n $ACR_NAME  --sku Standard
+AKS_CLUSTER_NAME=k8demoCluster-scale
+ACR_NAME=acrVerityk8sdemo-scale
+az acr create  -g k8demo-scale  -n $ACR_NAME  --sku Standard
 cd ratings-api
-az acr build  -g k8demo --registry acrVerityk8sdemo  --image ratings-api:v1 .
+az acr build  -g k8demo-scale --registry acrVerityk8sdemo-scale  --image ratings-api:v1 .
 cd ~
 cd CloudTraining/ratings-web
-az acr build -g k8demo --registry acrVerityk8sdemo  --image ratings-web:v1 .
-az acr repository list  --name acrVerityk8sdemo   --output table
+az acr build -g k8demo-scale --registry acrVerityk8sdemo-scale  --image ratings-web:v1 .
+az acr repository list  --name acrVerityk8sdemo-scale   --output table
 cd ~
 cd CloudTraining/
-az aks update --name $AKS_CLUSTER_NAME  -g k8demo --attach-acr $ACR_NAME
+az aks update --name $AKS_CLUSTER_NAME  -g k8demo-scale --attach-acr $ACR_NAME
