d40cdab506540768d0f28eceef79e91bf997f2cb
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-29 19:25:04 +0530

    Update 5_secret_mongo.sh

--- 5_secret_mongo.sh
+++ 5_secret_mongo.sh
@@ -1,3 +1,3 @@
-kubectl create secret generic mongosecret --namespace ratingsapp-scale \
+kubectl create secret generic mongosecret-scale --namespace ratingsapp-scale \
     --from-literal=MONGOCONNECTION="mongodb://cldtraining-scale:cldtraining-scale@ratings-scale-mongodb.ratingsapp:27017/ratingsdb-scale"
 kubectl describe secret mongosecret-scale --namespace ratingsapp-scale
