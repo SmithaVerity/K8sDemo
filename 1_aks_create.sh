067893c146611ac47577c3b34b42cb8fdf358f21
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-29 18:37:33 +0530

    Update 1_aks_create.sh

--- 1_aks_create.sh
+++ 1_aks_create.sh
@@ -8,8 +8,7 @@ WORKSPACE=k8demoworkspace-scale
 
 
 az group create   --name $RESOURCE_GROUP   --location $REGION_NAME
-az network vnet create  --resource-group $RESOURCE_GROUP   --location $REGION_NAME   --name $VNET_NAME  --address-prefixes 10.0.0.0/24 \ 
---subnet-name $SUBNET_NAME   --subnet-prefixes 10.0.0.0/29
+az network vnet create  --resource-group $RESOURCE_GROUP   --location $REGION_NAME   --name $VNET_NAME  --address-prefixes 10.0.0.0/24  --subnet-name $SUBNET_NAME   --subnet-prefixes 10.0.0.0/29
 SUBNET_ID=$(az network vnet subnet show   --resource-group $RESOURCE_GROUP  --vnet-name $VNET_NAME --name $SUBNET_NAME  --query id -o tsv)	
 VERSION=$(az aks get-versions  --location $REGION_NAME  --query 'orchestrators[?!isPreview] | [-1].orchestratorVersion'   --output tsv)
 
