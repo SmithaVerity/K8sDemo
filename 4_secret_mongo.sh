b0a8c900a7182ead45572aa47cccc78f32aa8102
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-29 19:34:46 +0530

    Update and rename 5_secret_mongo.sh to 4_secret_mongo.sh

---
+++ 4_secret_mongo.sh
@@ -0,0 +1,2 @@
+kubectl create secret generic mongosecret-scale --namespace ratingsapp-scale --from-literal=MONGOCONNECTION="mongodb://cldtraining-scale:cldtraining-scale@ratings-scale-mongodb.ratingsapp:27017/ratingsdb-scale"
+kubectl describe secret mongosecret-scale --namespace ratingsapp-scale
