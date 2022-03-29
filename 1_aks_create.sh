b487af54c179e781b9282dac4285b295f6e74698
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-03-29 18:51:17 +0530

    Update 1_aks_create.sh

--- 1_aks_create.sh
+++ 1_aks_create.sh
@@ -23,7 +23,7 @@ az aks create \
 --network-plugin azure \
 --vnet-subnet-id $SUBNET_ID \
 --service-cidr 10.2.0.0/30 \
---dns-service-ip 10.2.0.0 \
+--dns-service-ip 10.2.0.1 \
 --docker-bridge-address 172.17.0.1/16 \
 --generate-ssh-keys --y
 
