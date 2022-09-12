07b17bbc05a5160a6fe3e2b4a386091f6d5d5f77
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-09-12 17:41:30 +0530

    Update 3_mongodb.sh

--- 3_mongodb.sh
+++ 3_mongodb.sh
@@ -1,4 +1,5 @@
-helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo remove bitnami
+helm repo add bitnami  https://raw.githubusercontent.com/bitnami/charts/index/bitnami
 helm search repo bitnami
 helm install ratings bitnami/mongodb  --namespace ratingsapp \
     --set auth.username=cldtraining,auth.password=cldtraining,auth.database=ratingsdb
