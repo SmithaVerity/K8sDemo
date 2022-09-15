b636d2ba90f0717588447e58f4868711d8a16326
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-09-15 16:02:34 +0530

    Update and rename 9_install_ingress.sh to 8_install_ingress.sh

--- 8_install_ingress.sh
+++ 8_install_ingress.sh
@@ -1,6 +1,6 @@
 helm repo update
 helm install nginx-ingress ingress-nginx/ingress-nginx  --namespace ingress \
     --set controller.replicaCount=2 \
-    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
+    --set controller.nodeSelector."kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux
 kubectl get services --namespace ingress -w
