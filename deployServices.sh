0f62fc3b618ef99010533a9bbfc74568186bed91
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2022-09-15 15:54:51 +0530

    Update deployServices.sh

--- deployServices.sh
+++ deployServices.sh
@@ -38,6 +38,6 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm install nginx-ingress ingress-nginx/ingress-nginx  --namespace ingress \
     --set controller.replicaCount=2 \
-    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
+    --set controller.nodeSelector."kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux
 kubectl get services --namespace ingress -w
